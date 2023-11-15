55cc5d05b05660b4fd8350885de6b4e1c22ba33c
User564397 <toriread33@gmail.com>
2023-11-15 13:18:10 +0300

    main

--- pyrrr/main.py
+++ pyrrr/main.py
@@ -1,25 +1,7 @@
 def print_hi(name):
     print(f'Hi, {name}')
 
-
-def calc(a, b):
-    print(a + b)
-    print(a - b)
-    print(a * b)
-    print(a / b)
-
-
 if __name__ == '__main__':
-    name = input('Введте своё имя: ')
-    print_hi(name)
     a, b = list(map(int, input().split()))
-    calc(a, b)
-
-    print('Введите 2 числа через пробел: ')
-    a, b = list(map(int, input().split(' ')))
-    print(a + b)
-    print(a - b)
-    print(a * b)
-    print(a / b)
-
+    print_hi('oleg')
     print('Изменение')
