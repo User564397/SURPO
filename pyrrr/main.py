fe5bb24ee8e080f9f481eaa6e3149ef0ebfc7310
User564397 <toriread33@gmail.com>
2023-11-15 13:42:24 +0300

    main chhhh

--- pyrrr/main.py
+++ pyrrr/main.py
@@ -1,9 +1,18 @@
 def print_hi(name):
     print(f'Hi, {name}')
 
+
+def calc(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a / b)
+
+
 if __name__ == '__main__':
+    name = input('Введте своё имя: ')
+    print_hi(name)
     a, b = list(map(int, input().split()))
-    print_hi('oleg')
-    print('Изменение')
+    calc(a, b)
 
     k = 34
