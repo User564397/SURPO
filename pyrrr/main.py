7fc3636c6a729cba641954b7b9fe78789ad9a451
User564397 <toriread33@gmail.com>
2023-11-16 00:44:03 +0300

    pepega

--- pyrrr/main.py
+++ pyrrr/main.py
@@ -1,18 +1,4 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+def pepega(name):
+    return f"{name} - пепега"
 
-
-def calc(a, b):
-    print(a + b)
-    print(a - b)
-    print(a * b)
-    print(a / b)
-
-
-if __name__ == '__main__':
-    name = input('Введте своё имя: ')
-    print_hi(name)
-    a, b = list(map(int, input().split()))
-    calc(a, b)
-
-    k = 34
+print(pepega(input('Введите имя: ')))
